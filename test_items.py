294fb18e1c6f3d969f4d31993d9b22f5e1b3d125
Wildlepesha <79692261+Wildlepesha@users.noreply.github.com>
2021-06-20 10:16:59 +0300

    Initial commit

--- test_items.py
+++ test_items.py
@@ -9,6 +9,13 @@ class TestStepic():
         browser.set_window_size("1720", "900")
 
         button = browser.find_element_by_class_name("btn-add-to-basket").click()
-        text = browser.find_element_by_css_selector("#messages > div:nth-child(1) > div > strong").text
 
-        assert text == "Coders at Work", f"Wrong book was added, expected Coders at Work, but got {text}"
+        # На этом начинается мои фантазии, потому что поиск кнопки слишком скучно и ассерт не будет срабатывать,
+        # если элемент не будет найден, поэтому я решил проверить на сообщение об успешном добавлении товара в корзину
+
+        alert_success = browser.find_element_by_css_selector("#messages > div:nth-child(1)")
+        alert_success_class = alert_success.get_attribute("class")
+
+        # time.sleep(30)
+
+        assert "alert-success" in alert_success_class, "Expected to find alert-success bootstrap attribute"
